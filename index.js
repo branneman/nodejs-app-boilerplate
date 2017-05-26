abdc2174c1f6ce26e5bdd78ebe14a353fd233951
Bran van der Meer <branmovic@gmail.com>
2017-05-26 23:23:43 +0200

    Initial working implementation of http2 with server push

--- index.js
+++ index.js
@@ -1,75 +1,64 @@
 'use strict';
 
 const fs = require('fs');
+const http2 = require('http2');
 const express = require('express');
 
+// Dirty hack until Express supports http2.
+// See: https://github.com/expressjs/express/issues/2761
+express.request.__proto__ = http2.IncomingMessage.prototype;
+express.response.__proto__ = http2.ServerResponse.prototype;
+
+
 /**
- * Generic functions
+ * Generic util functions
  */
 const read = f => fs.readFileSync(`${__dirname}/${f}`, { encoding: 'utf8' }).toString();
 const render = (tplStr, viewBagTuples) =>
     viewBagTuples.reduce(
-        (prev, [key, val]) => prev.replace(new RegExp(`\{\{\s*${key}\s*\}\}`, 'i'), val),
+        (prev, [key, val]) => prev.replace(`{{ ${key} }}`, val),
         tplStr
     );
+
 
 /**
  * Config
  */
-const serverPort = 8080;
+const serverPort = 8443;
 const serverOptions = { key: read('cert/key.pem'), cert: read('cert/cert.pem') };
 const baseViewBag = [
     ['loadCSScore', read('src/static/js/loadCSS.min.js')],
     ['loadCSSrelPreload', read('src/static/js/cssrelpreload.min.js')]
 ];
 
-/**
- * HTTP/1 Server
- */
-const appHTTP1 = express();
-appHTTP1.use((req, res) => {
-    console.log('GET / HTTP/1');
-    res.redirect(301, `https://localhost:${serverPort}${req.path}`);
-});
-require('http')
-    .createServer(serverOptions, appHTTP1)
-    .listen(serverPort, () => console.log('HTTP/1 server listening'));
-
-/**
- * HTTP/1+SSL Server
- */
-const appHTTP1ssl = express();
-
-appHTTP1ssl.get('/', (req, res) => {
-    console.log('GET / HTTP/1+SSL');
-
-    const viewBag = baseViewBag.concat([
-        ['criticalCSS', read('src/homepage/critical.css')]
-    ]);
-    res.send(render(read('src/homepage/http1.html'), viewBag));
-
-});
-
-require('https')
-    .createServer(serverOptions, appHTTP1ssl)
-    .listen(serverPort, () => console.log('HTTP/1+SSL server listening'));
 
 /**
  * HTTP/2 Server
  */
-const appHttp2 = express();
+const app = express();
+app.disable('x-powered-by');
+app.disable('etag');
+app.use('/static', express.static(`${__dirname}/src`));
 
-appHttp2.get('/', (req, res) => {
-    console.log('GET / HTTP/2');
+app.get('/', (req, res) => {
 
-    res.push(read('src/homepage/remaining.css'), {
-        url: 'src/homepage/remaining.css'
-    });
+    // HTTP/2 response with Critical CSS via server push
+    if (req.httpVersion === '2.0') {
 
-    res.send(render(read('src/homepage/http2.html'), baseViewBag));
+        const push = res.push('/static/homepage/critical.css');
+        push.writeHead(200);
+        fs.createReadStream(`${__dirname}/src/homepage/critical.css`).pipe(push);
+
+        return res.send(render(read(`src/homepage/http2.html`), baseViewBag));
+
+    }
+
+    // HTTP/1 response with Critical CSS inlined
+    const viewBag = baseViewBag.concat(['criticalCSS', read('src/homepage/critical.css')]);
+    res.send(render(read(`src/homepage/http1.html`), viewBag));
 
 });
 
-require('http2')
-    .createServer(serverOptions, appHttp2)
-    .listen(serverPort, () => console.log('HTTP/2 server listening'));
+http2.createServer(serverOptions, app)
+    .listen(serverPort)
+    .on('listening', () => console.log(`HTTP/2 server listening on ${serverPort}`));
